6d9543d6173ae14e8f1d950ce9a9ea081c91e0d3
Denis Kiryukhin <den.kiryukhin@gmail.com>
2026-05-26 15:38:26 +0200

    nothing to see here!!!!!!!!!!!!1

--- potential-flow-run.py
+++ potential-flow-run.py
@@ -3,12 +3,22 @@ import numpy as np
 
 ax = plt.figure().add_subplot(projection='3d')
 
-x, y, z = np.meshgrid(np.linspace(-10,10,100),
-                      np.linspace(-10,10,100),
-                      np.linspace(-10,10,100))
-u = np.linspace(-10,10,100)
-v = x**2 
-w = x**3
+x, y, z = np.meshgrid(np.linspace(-10,10,10),
+                      np.linspace(-10,10,10),
+                      np.linspace(-10,10,10))
 
-ax.quiver(x,y,z,u,v,w)
-plt.show()
+def source(x,y,z, x0,y0,z0,m):
+    return m/(2*np.pi)*np.log( np.sqrt((x-x0)**2 + (y-y0)**2 + (z-z0)**2) )
+
+def vel_potental(x,y,z):
+    return source(x,y,z, 0, 1, 1, 100) + source(x,y,z, 0, 5, 1, -10)
+
+def derivative_x(vel_potential, axis):
+    h = (vel_potential[1,:] - vel_potential[0,:])
+    return (vel_potential[1:,:] - vel_potential)/h #fwd difference
+
+
+print(vel_potental(x,y,z).shape)
+# ax.quiver(x, y, z, u, v, w, length=0.1, normalize=True)
+# plt.plot(x,y)
+# plt.show()
